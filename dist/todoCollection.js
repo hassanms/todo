e3fb6998c56e3bc1e80e925d221b49e46fc51cf4
Hassan MS <1engr.hassan@gmail.com>
2022-10-10 19:58:09 +0500

    Adding Features to the Collection Class

--- dist/todoCollection.js
+++ dist/todoCollection.js
@@ -7,17 +7,20 @@ class TodoCollection {
         this.userName = userName;
         this.todoItems = todoItems;
         this.nextId = 1;
-        // no statements required
+        this.itemMap = new Map();
+        todoItems.forEach(item => this.itemMap.set(item.id, item));
     }
     addTodo(task) {
         while (this.getTodoById(this.nextId)) {
             this.nextId++;
         }
-        this.todoItems.push(new todoItem_1.TodoItem(this.nextId, task));
+        // this.todoItems.push(new TodoItem(this.nextId, task));
+        this.itemMap.set(this.nextId, new todoItem_1.TodoItem(this.nextId, task));
         return this.nextId;
     }
     getTodoById(id) {
-        return this.todoItems.find(item => item.id === id);
+        // return this.todoItems.find(item => item.id === id);
+        return this.itemMap.get(id);
     }
     markComplete(id, complete) {
         const todoItem = this.getTodoById(id);
