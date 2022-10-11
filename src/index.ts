e104c0ff63cdabf02dccd7f72f895fc1d74297b0
Hassan MS <1engr.hassan@gmail.com>
2022-10-11 10:43:32 +0500

    Using a Third-Party Package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
@@ -7,7 +8,29 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection("Hassan", todos);
 
-console.clear();
-console.log(`${collection.userName}'s Todo List `
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
     + `(${ collection.getItemCounts().incomplete } items to do)`);
-collection.getTodoItems(true).forEach(item => item.printDetails());
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+
+enum Commands {
+    Quit = "Quit"
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+
+promptUser();
